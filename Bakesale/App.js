231147be17ef0eca22088898edcd8036e68527ca
dylanmoran1 <114390231+dylanmoran1@users.noreply.github.com>
2024-05-04 20:44:17 +0100

    Adding basic app content

--- Bakesale/App.js
+++ Bakesale/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar, Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <Text style={styles.header}>Bakesale</Text>
+          <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  header: {
+    fontSize: 40
+  }
+})
+
+
+export default App;
